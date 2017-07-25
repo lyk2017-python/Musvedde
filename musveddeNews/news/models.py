f02a27c26ce37ec2720afa7e49508be1eee9daf2
Hakan Yılmaz <gshakan16@gmail.com>
2017-07-25 19:33:36 +0300

    Migrations işlemleri yapıldı

--- musveddeNews/news/models.py
+++ musveddeNews/news/models.py
@@ -1,3 +1,25 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=160)
+    content = models.TextField()
+    image = models.ImageField(null=True, blank=True, default=None)
+    liked = models.IntegerField(default=0)
+    reported = models.PositiveIntegerField(default=0)
+    categories = models.ForeignKey("Category")
+    tags = models.ManyToManyField("Tags")
+    slug = models.SlugField(max_length=160)
+    created_at = models.DateTimeField(default=datetime.datetime.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    featured_until = models.DateTimeField(default=None, blank=True, null=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    parent = models.ForeignKey("self", default=0)
+
+
+class Tags(models.Model):
+    tag = models.CharField(max_length=50)
