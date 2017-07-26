2987d26fd88ddab3d7fb2d631d166e1b365cd643
Hakan Yılmaz <gshakan16@gmail.com>
2017-07-26 14:09:12 +0300

    Dökümantasyon ve str fonksiyonları yazıldı

--- musveddeNews/news/models.py
+++ musveddeNews/news/models.py
@@ -1,6 +1,8 @@
 import datetime
 from django.db import models
 
+""" Class for Post News """
+
 
 class Post(models.Model):
     title = models.CharField(max_length=160)
@@ -15,11 +17,26 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     featured_until = models.DateTimeField(default=None, blank=True, null=True)
 
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+""" Class for Categories """
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
     parent = models.ForeignKey("self", default=0)
 
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+""" Class for Tags """
+
 
 class Tags(models.Model):
     tag = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "{}".format(self.name)
--- musveddeNews/news/views.py
+++ musveddeNews/news/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post, Category
+from datetime import datetime, timedelta
 
 # Create your views here.
+
+
+class HomeView(generic.ListView):
+    def get_queryset(self):
+        one_week_ago = datetime.today() - timedelta(days=7)
+        return Post.objects.filter(created_at__gte=one_week_ago)
+
+
+class CategoryView(generic.DetailView):
+    def get_queryset(self, pk):
+        category = Category.objects.get(id=pk)
+        return Post.objects.filter(categories=category)
+
+
+class NewsView(generic.DetailView):
+    def get_queryset(self):
+        return Post.objects.filter()
