05925809d736865b4cbe4979c5008ad2c185c05e
Hakan Yılmaz <gshakan16@gmail.com>
2017-07-27 14:10:24 +0300

    Admin paneline giriş yapıldı

--- musveddeNews/news/admin.py
+++ musveddeNews/news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from news.models import Category
 
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
